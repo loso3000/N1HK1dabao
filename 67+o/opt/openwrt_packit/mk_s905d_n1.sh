21969a4b0e74e381a7f28f7402cba84c659c9bd6
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-12-16 12:14:22 +0800

    nn

--- 67+o/opt/openwrt_packit/mk_s905d_n1.sh
+++ 67+o/opt/openwrt_packit/mk_s905d_n1.sh
@@ -34,7 +34,7 @@ check_file ${OPWRT_ROOTFS_GZ}
 echo "Use $OPWRT_ROOTFS_GZ as openwrt rootfs!"
 
 # 目标镜像文件
-TGT_IMG="${WORK_DIR}/N1-jcnf-mini-67+o.img"
+TGT_IMG="${WORK_DIR}/N1-novpn-67+o.img"
 
 # 补丁和脚本
 ###########################################################################
